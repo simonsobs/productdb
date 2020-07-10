d1a241c660aa38f908092825bc5bdf74c5918691
Tai Sakuma <tai.sakuma@gmail.com>
2020-07-10 12:33:21 -0400

    add test_version.py

---
+++ tests/test_version.py
@@ -0,0 +1,9 @@
+import productdb
+
+##__________________________________________________________________||
+def test_version():
+    """test if the version string is attached to the module
+    """
+    productdb.__version__
+
+##__________________________________________________________________||
